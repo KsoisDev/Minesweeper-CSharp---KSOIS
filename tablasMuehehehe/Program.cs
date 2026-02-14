a71abdf3cc3ca304f5fac31528979be1d224c67c
KSOIS <kitylive112@gmail.com>
2026-02-14 14:11:39 +0100

    funcion de ganar, desmarcar y marcar

--- tablasMuehehehe/Program.cs
+++ tablasMuehehehe/Program.cs
@@ -11,28 +11,126 @@ class Program
 	static String[,] tabla;
 	static String[,] tablaVisible;
     static bool perder = false;
+	static int numeroDeMinas = 0;
+    static int minasMarcadas = 0; //minas correctamente marcadas
+    static int minasMarcadasTotales = 0; //minas marcadas
 	static void Main(string[] args)
 	{
 		tabla = pedirMatriz();
+		//MarcarTabla();
+		bool ganar = false;
+		while (!ganar || perder)
+		{
+			Console.WriteLine("Quieres descubrir o quieres marcar? \n marcar: 1 descubri: 2 desmarcar: 3");
+			int input = int.Parse(Console.ReadLine());
+			switch (input)
+			{
+				case 1: MarcarTabla(); break;
+				case 2: DescubrirTabla(); break;
+				case 3: DesmarcarTabla(); break;
+				 default: Console.WriteLine("Error"); break;
+			}
+			ValidarMinas(tabla, tablaVisible);
+			ganar = ValidarVictoria(tablaVisible, numeroDeMinas, minasMarcadas);
+		}
+
+		Console.WriteLine("has ganado!");
+		//DescubrirTabla();
 		//mostrarMatriz(tabla);
+
+	}
+
+	static void DesmarcarTabla()
+	{
+		Console.WriteLine("cuantas minas quieres desmarcar");
+		int numeroDeMinas = int.Parse(Console.ReadLine());
+		for (int i = 0; i < numeroDeMinas; i++)
+		{
+			Console.WriteLine("Coordenada:");
+			String input = Console.ReadLine();
+			String[] dimensiones = input.Split(' ');
+			int filas = int.Parse(dimensiones[0]) - 1;
+			int columnas = int.Parse(dimensiones[1]) - 1;
+			if ((tablaVisible[filas, columnas] == "P"))
+			{
+				tablaVisible[filas, columnas] = "X";
+				minasMarcadas--;
+				minasMarcadasTotales--;
+			}
+			else
+			{
+				Console.WriteLine("Esta casilla no esta marcada.");
+			}
+		}
+		mostrarMatriz(tablaVisible);
+	}
+
+	static void MarcarTabla()
+	{
+		Console.WriteLine("cuantas minas quieres marcar");
+		int numeroDeMinas = int.Parse(Console.ReadLine());
+		for (int i = 0; i < numeroDeMinas; i++)
+		{
+			Console.WriteLine("Coordenada:");
+			String input = Console.ReadLine();
+			String[] dimensiones = input.Split(' ');
+			int filas = int.Parse(dimensiones[0]) - 1;
+			int columnas = int.Parse(dimensiones[1]) - 1;
+			if (!(tablaVisible[filas, columnas] == "P"))
+			{
+				tablaVisible[filas, columnas] = "P";
+				minasMarcadasTotales++;
+				if (tabla[filas, columnas] == "*")
+					minasMarcadas++;
+			}
+			else
+			{
+				Console.WriteLine("Esta casilla ya esta marcada.");
+			}
+		}
+		mostrarMatriz(tablaVisible);
+	}
+
+	static bool ValidarVictoria(String[,] matrizVisible, int minasTotales, int minasMarcadas)
+	{
+		if (!perder)
+		{
+			bool todoDescubierto = true;
+			for (int i = 0; i < matrizVisible.GetLength(0); i++)
+			{
+				for(int j = 0; j<matrizVisible.GetLength(1); j++)
+				{
+					if (matrizVisible[i, j] == "X")
+						todoDescubierto = false;
+				}
+			}
+
+			if (todoDescubierto && minasMarcadas == minasTotales && minasMarcadasTotales == minasTotales)
+				return true;
+		}
+
+		return false;
+	}
+	static void DescubrirTabla()
+	{
 		Console.WriteLine("cuantas minas quieres descubrir");
 		int numeroDeMinas = int.Parse(Console.ReadLine());
 		for (int i = 0; i < numeroDeMinas; i++)
 		{
-				Console.WriteLine("Coordenada:");
-				String input = Console.ReadLine();
-				String[] dimensiones = input.Split(' ');
-				int filas = int.Parse(dimensiones[0]) - 1;
-				int columnas = int.Parse(dimensiones[1]) - 1;
-				DescubrirMatriz(filas, columnas);
-				Console.WriteLine(perder);
-				Console.WriteLine("Has perdido");
+			Console.WriteLine("Coordenada:");
+			String input = Console.ReadLine();
+			String[] dimensiones = input.Split(' ');
+			int filas = int.Parse(dimensiones[0]) - 1;
+			int columnas = int.Parse(dimensiones[1]) - 1;
+			DescubrirMatriz(filas, columnas);
+			Console.WriteLine(perder);
+			Console.WriteLine("Has perdido");
 		}
 		mostrarMatriz(tablaVisible);
+	
 		if(perder) Console.WriteLine("has perdido."); 
 		else Console.WriteLine("Pues no has perdido");
 	}
-
 	static void DescubrirMatriz(int fila, int columna)
 	{
 		int numeroMinas = 0;
@@ -113,18 +211,19 @@ class Program
 		}
 	}
 
-	static int ValidarMinas(String[,] matriz)
+	static void ValidarMinas(String[,] matriz, String[,] matrizvisible)
 	{
-		int numeroDeMinas = 0;
+		numeroDeMinas = 0;
 		for (int i = 0; i < matriz.GetLength(0); i++)
 		{
 			for (int j = 0; j < matriz.GetLength(1); j++)
 			{
 				if (matriz[i, j] == "*")
+				{
 					numeroDeMinas++;
+				}
 			}
 		}
-		return numeroDeMinas;
 	}
 	static String[,] pedirMatriz()
 	{
